259a737e86d8783ca4b87a8d62c1d5b3f9743ea7
Bhavya Lakhani <bhavyalakhani89@gmail.com>
2024-11-22 20:41:19 -0500

    signup

--- src/Kanbas/Account/SignUp.tsx
+++ src/Kanbas/Account/SignUp.tsx
@@ -1,59 +1,32 @@
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { setCurrentUser } from "./reducer";
-import { useDispatch } from "react-redux";
-import * as db from "../Database";
+import { Link } from "react-router-dom";
 
-export default function Signin() {
-  const [credentials, setCredentials] = useState<any>({});
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const signin = () => {
-    const user = db.users.find(
-      (u: any) =>
-        u.username === credentials.username &&
-        u.password === credentials.password
-    );
-    if (!user) return;
-    dispatch(setCurrentUser(user));
-    navigate("/Kanbas/Dashboard");
-  };
-
+export default function Signup() {
   return (
-    <div id="wd-signin-screen">
-      <h1>Sign in</h1>
-
+    <div id="wd-signup-screen">
+      <h3>Sign up</h3>
       <input
-        defaultValue={credentials.username}
-        onChange={(e) =>
-          setCredentials({ ...credentials, username: e.target.value })
-        }
         id="wd-username"
         placeholder="username"
         className="form-control mb-2"
       />
-
       <input
-        defaultValue={credentials.password}
-        onChange={(e) =>
-          setCredentials({ ...credentials, password: e.target.value })
-        }
         id="wd-password"
         placeholder="password"
-        type="password"
         className="form-control mb-2"
+        type="password"
       />
-
-      <button
-        onClick={signin}
-        id="wd-signin-btn"
-        className="btn btn-primary w-100"
-      >
-        Sign in{" "}
-      </button>
-      <Link id="wd-signup-link" to="/Kanbas/Account/Signup">
-        Sign up
+      <input
+        id="wd-password"
+        placeholder="verify password"
+        className="form-control mb-3"
+        type="password"
+      />
+      <Link to="/Kanbas/Account/Profile" className="btn btn-primary w-100 mb-2">
+        {" "}
+        Sign up{" "}
+      </Link>
+      <Link to="/Kanbas/Account/Signin" className="btn btn-primary w-100">
+        Sign in
       </Link>
     </div>
   );
